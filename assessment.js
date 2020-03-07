6bb89e7d5159a34958d44342074e8a39fe01bcc8
dogsoclopp <61900668+dogsoclopp@users.noreply.github.com>
2020-03-07 19:44:59 +0900

    Update assessment.js

--- assessment.js
+++ assessment.js
@@ -5,9 +5,9 @@ const resultDivided = document.getElementById('result-area');
 const tweetDivided = document.getElementById('tweet-area');
 
 /**
-* 指定した要素の子どもを全て除去する
-* @param {HTMLElement} element HTMLの要素
-*/
+ * 指定した要素の子どもを全て除去する
+ * @param {HTMLElement} element HTMLの要素
+ */
 function removeAllChildren(element) {
   while (element.firstChild) { // 子どもの要素があるかぎり除去
     element.removeChild(element.firstChild);
@@ -19,8 +19,8 @@ assessmentButton.onclick = () => {
   if (userName.length === 0) { // 名前が空の時は処理を終了する
     return;
   }
-  
-  // 診断結果表示エリア作成
+
+  // 診断結果表示エリアの作成
   removeAllChildren(resultDivided);
   const header = document.createElement('h3');
   header.innerText = '診断結果';
@@ -35,7 +35,7 @@ assessmentButton.onclick = () => {
   removeAllChildren(tweetDivided);
   const anchor = document.createElement('a');
   const hrefValue = 'https://twitter.com/intent/tweet?button_hashtag='
-    + encodeURIComponent('あなたのいいところ')　
+    + encodeURIComponent('あなたのいいところ')
     + '&ref_src=twsrc%5Etfw';
   anchor.setAttribute('href', hrefValue);
   anchor.className = 'twitter-hashtag-button';
@@ -65,25 +65,25 @@ const answers = [
   '{userName}のいいところは好奇心です。新しいことに向かっていく{userName}の心構えが多くの人に魅力的に映ります。',
   '{userName}のいいところは気配りです。{userName}の配慮が多くの人を救っています。',
   '{userName}のいいところはその全てです。ありのままの{userName}自身がいいところなのです。',
-  '{userName}のいいところは自制心です。やばいと思ったときにしっかりと衝動を抑えられる{userName}が皆から評価されています。',
-  '{userName}のいいろころは優しさです。{userName}の優しい雰囲気や立ち振る舞いに多くの人が癒されています。'
+  '{userName}のいいところは自制心です。やばいと思ったときにしっかりと衝動を抑えられる{userName}が皆から評価されています。'
 ];
 
 /**
-* 名前の文字列を渡すと診断結果を返す関数
-* @param {string} userName ユーザーの名前
-* @return {string} 診断結果
-*/
+ * 名前の文字列を渡すと診断結果を返す関数
+ * @param {string} userName ユーザーの名前
+ * @return {string} 診断結果
+ */
 function assessment(userName) {
   // 全文字のコード番号を取得してそれを足し合わせる
   let sumOfcharCode = 0;
   for (let i = 0; i < userName.length; i++) {
     sumOfcharCode = sumOfcharCode + userName.charCodeAt(i);
   }
-  
+
   // 文字のコード番号の合計を回答の数で割って添字の数値を求める
   const index = sumOfcharCode % answers.length;
   let result = answers[index];
+
   result = result.replace(/{userName}/g, userName);
   return result;
 }
